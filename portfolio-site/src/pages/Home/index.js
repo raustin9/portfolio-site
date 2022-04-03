a6b99e06862b442d1e8e09e51724da0d360def6c
Reagan Austin <raustin9@vols.utk.edu>
2022-04-02 23:53:23 -0400

    fix compiler warning

--- portfolio-site/src/pages/Home/index.js
+++ portfolio-site/src/pages/Home/index.js
@@ -1,7 +1,6 @@
-import React, { useState , useEffect} from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import * as FaIcons from 'react-icons/fa'
-// import pixelart from './pixelart3.png';
 import pixelimg from './pixelart2.jpg'
 import './home.css'
 
