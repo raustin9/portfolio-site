c95bba8b3978d192d216f4f05227e398b4d29387
Reagan Austin <raustin9@vols.utk.edu>
2022-04-11 20:45:03 -0400

    add to about me page

--- src/pages/AboutMe/index.js
+++ src/pages/AboutMe/index.js
@@ -18,8 +18,58 @@ const AboutMe = () => {
         <h1 className="aboutme-section-title">general</h1>
       </div> */}
       <div className="aboutme">
-        <div className="aboutme-blurb">
+        {/* <div className="aboutme-blurb">
           <h1 className="aboutme-text">Hello everyone! My name is Reagan and I am a Computer Science student at the University of Tennessee at Knoxville, TN. I program a lot in my spare time, and I usually work in C++, C, or whatever specific tool I am using. I also really enjoy making video games, and I use Game Maker Studio 2 for that, and I use FL Studio to make the music and sound effects. I make music on my own outside of projects anyway, and you can see that elsewhere on this site. I am in the group Hack4Impact which builds applications for local non profit organnizations in Knoxville and East Tennessee, and that is where a lot of my time developing outside of school goes.</h1>
+        </div> */}
+        <div className="aboutme-general">
+          <h1 className="aboutme-text">Hello everyone! My name is Reagan, and I am a Computer Science student at the University of Tennessee Knoxville. In my spare time, I love to program, produce music, and make video games. For most of my hobbies, you can see some of my work here on this site. </h1>
+          <div className="aboutme-grid">
+            <div className="aboutme-grid-item" id="aboutme-education">
+              <div className="aboutme-grid-title">
+                <h1 className="aboutme-text" >Education</h1>
+              </div>
+              <p className="aboutme-text-p">I am currently a sophomore at the University of Tennessee in Knoxville, TN, and I major in Computer Science there.</p>
+              <p className="aboutme-text-p">Some major courses:</p>
+              <ul className="aboutme-education-courses">
+                <li className="aboutme-education-courses-item">COSC 102 - Introduction to Computer Science</li>
+                <li className="aboutme-education-courses-item">COSC 130 - Computer Organization</li>
+                <li className="aboutme-education-courses-item">COSC 202 - Data Structures and Algorithms I</li>
+                <li className="aboutme-education-courses-item">COSC 302 - Data Structures and ALgorithms II</li>
+                <li className="aboutme-education-courses-item">COSC 311 - Discrete Structures</li>
+              </ul>
+            </div>
+            <div className="aboutme-grid-item" id="aboutme-skills">
+              <div className="aboutme-grid-title">
+                <h1 className="aboutme-text" >Skills</h1>
+              </div>
+              <ul className="aboutme-education-courses">
+                <li className="aboutme-education-courses-item">C++ - I am very confident in this, and I learned most of it through my time at UTK</li>
+                <li className="aboutme-education-courses-item">REACT - I learned all of my skills in REACT on my own, and I have learned a lot. You can see the results of it on this website and some other projects</li>
+                <li className="aboutme-education-courses-item">Game Maker Studio 2 - I use this to make video games. It is perfect as it is specialized in 2D games which are what I enjoy making the most</li>
+                <li className="aboutme-education-courses-item">FL Studio - This is what I use to make music. I have used it a good amount and built confidence in it</li>
+                <li className="aboutme-education-courses-item">UNIX - I use the unix terminal all the time, and I have gained a lot of experience with it through my time at UTK as well</li>
+                <li className="aboutme-education-courses-item">MIPS Assembly - I learned this in my Computer Organization class at UTK. It was easily my favorite part of the year and a lot of fun to use</li>
+                <li className="aboutme-education-courses-item">Styled Components - I used this to make the prototype of this website. I didn't include it in this one, but I may in the future because I think they are fun to use</li>
+                <li className="aboutme-education-courses-item">Typescript - I learned this through my projects at Hack4Impact. It makes a lot of sense when you have to deal with backend things</li>
+              </ul>
+            </div>
+            <div className="aboutme-grid-item" id="aboutme-hobbies">
+              <div className="aboutme-grid-title">
+                <h1 className="aboutme-text" >Hobbies</h1>
+              </div>
+              <ul className="aboutme-education-courses">
+                <li className="aboutme-education-courses-item">Video Game Design  -  I do this in my spare time when I can, and I like to make 2D rpg games. I do not have much work to my name because I don't normally put it in the public, but I'm working on something really cool right now :)</li>
+                <li className="aboutme-education-courses-item">Music Production  -  I love making music for both my video games and for whatever. I have not released anything in a long time, but I still make some stuff. Again, more will be coming out with the video game</li>
+                <li className="aboutme-education-courses-item">Hack4Impact  -  This is a group at UTK that builds software apps for non-profit organizations in East Tennessee. I spend a lot of time working on my team for them, and it is a lot of fun</li>
+                <li className="aboutme-education-courses-item">General Programming  -  I make small projects here and there for fun. The favorite one is the Image Processing project that I did, and that one allows the user to input an image, and encode a secret message to it, or decode a message from one</li>
+              </ul>
+            </div>
+            <div className="aboutme-grid-item" id="aboutme-other">
+              <div className="aboutme-grid-title">
+                {/* <h1 className="aboutme-text" ></h1> */}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <Resume />
