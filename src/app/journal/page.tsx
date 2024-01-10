98bfb75147e03a4899fc825492204812712c330e
raustin9 <raustin9@vols.utk.edu>
2024-01-10 16:15:29 -0500

    add placeholders for music and journal pages

--- src/app/journal/page.tsx
+++ src/app/journal/page.tsx
@@ -1,7 +1,25 @@
+import { Box, Typography } from "@mui/material";
+
 export default function JournalPage() {
   return (
     <>
-      <h1>Journal</h1>
+      <Box
+        sx={{
+          display: 'flex',
+          height: '50%',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Typography
+          sx={{
+            fontSize: '4rem',
+            color: '#fff',
+            fontFamily: 'serif',
+          }}>
+            Journal Coming Soon!
+          </Typography>
+      </Box>
     </>
   )
 }
--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -1,3 +1,25 @@
+import { Box, Typography } from "@mui/material";
+
 export default function MusicPage() {
-  return <h1>Music</h1>
+  return (
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          height: '50%',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Typography
+          sx={{
+            fontSize: '4rem',
+            color: '#fff',
+            fontFamily: 'serif',
+          }}>
+            Music Coming Soon!
+          </Typography>
+      </Box>
+    </>
+  )
 }
